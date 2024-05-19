cfdf92c7e987cb601e58bf89747c1fff3dfa17d3
rafab15 <rafaelberenguel15@gmail.com>
2024-05-19 12:38:04 -0300

    Agrego las asociaciones correspondientes a las nuevas tablas

--- src/models/associations.js
+++ src/models/associations.js
@@ -4,6 +4,18 @@ import { Group } from "./Group.js";
 
 User.hasMany(GroupMember, { foreignKey: 'user_id' });
 User.hasMany(Group, {foreignKey:'admin_id'});
+User.hasMany(IndividualExpense, { foreignKey: 'user_id' });
+
 Group.hasMany(GroupMember, { foreignKey: 'group_id' });
+Group.hasMany(Expense, { foreignKey: 'group_id' });
+Group.hasMany(IndividualExpense, { foreignKey: 'group_id' });
+
 GroupMember.belongsTo(User, { foreignKey: 'user_id' });
 GroupMember.belongsTo(Group, { foreignKey: 'group_id' });
+
+Expense.belongsTo(Group, { foreignKey: 'group_id' });
+Expense.hasMany(IndividualExpense, { foreignKey: 'expense_id' });
+
+IndividualExpense.belongsTo(User, { foreignKey: 'user_id' });
+IndividualExpense.belongsTo(Expense, { foreignKey: 'expense_id' });
+IndividualExpense.belongsTo(Group, { foreignKey: 'group_id' });
